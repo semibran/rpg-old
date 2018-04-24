74c397f4e32bafef63b6a33af92742a47f39a863
Brandon Semilla <semibran@gmail.com>
2018-04-24 11:19:46 -0700

    organize helper functions

--- src/constants.js
+++ src/constants.js
@@ -18,3 +18,27 @@ module.exports = {
 		}
 	}
 }
+
+// thief
+// > thief:   1
+// > knight:  0
+// > warrior: 1
+// > mage:    2
+
+// knight
+// > knight:  1
+// > warrior: 1
+// > thief:   2
+// > mage:    2
+
+// warrior
+// > warrior: 2
+// > mage:    2
+// > knight:  2
+// > thief:   0
+
+// mage
+// > mage:    1
+// > thief:   1
+// > knight:  2
+// > warrior: 2
--- src/index.js
+++ src/index.js
@@ -2,68 +2,28 @@ import loadImage from "img-load"
 import disassemble from "./sprites"
 import maps from "./maps"
 import View from "./view"
+import pixels from "../lib/pixels"
+import extract from "../lib/img-extract"
+import Canvas from "../lib/canvas"
 
 loadImage("sprites.png")
-  .then(main)
+	.then(main)
 
 let state = {
-  map: maps.test,
-  cursor: {
-    position: null,
-    selection: null
-  }
-}
-
-function get(image, x, y) {
-  let i = (y * image.width + x) * 4
-  let r = image.data[i]
-  let g = image.data[i + 1]
-  let b = image.data[i + 2]
-  let a = image.data[i + 3]
-  return [ r, g, b, a ]
-}
-
-function replace(image, oldColor, newColor) {
-  for (var i = 0; i < image.data.length; i += 4) {
-    for (var c = 0; c < 4; c++) {
-      if (image.data[i + c] !== oldColor[c]) {
-        break
-      }
-    }
-
-    if (c !== 4) {
-      continue
-    }
-
-    for (var c = 0; c < 4; c++) {
-      image.data[i + c] = newColor[c]
-    }
-  }
-}
-
-function extract(image, x, y, width, height) {
-  var canvas = document.createElement("canvas")
-  var context = canvas.getContext("2d")
-  canvas.width = width
-  canvas.height = height
-  context.drawImage(image, -x, -y)
-  return canvas
-}
-
-function Canvas(width, height) {
-  var canvas = document.createElement("canvas")
-  canvas.width = width
-  canvas.height = height
-  return canvas.getContext("2d")
+	map: maps.test,
+	cursor: {
+		position: null,
+		selection: null
+	}
 }
 
 function main(spritesheet) {
-  let { map, cursor } = state
-  let sprites = disassemble(spritesheet)
-  sprites.squares = {
-    attack: extract(sprites.squares, 16, 0, 16, 16),
-    move: extract(sprites.squares, 0, 0, 16, 16)
-  }
+	let { map, cursor } = state
+	let sprites = disassemble(spritesheet)
+	sprites.squares = {
+		attack: extract(sprites.squares, 16, 0, 16, 16),
+		move: extract(sprites.squares, 0, 0, 16, 16)
+	}
 
 	sprites.arrows = {
 		left:      extract(sprites.arrows,  0,  0, 16, 16),
@@ -82,103 +42,114 @@ function main(spritesheet) {
 		vert:      extract(sprites.arrows, 16, 48, 16, 16),
 	}
 
-  sprites.pieces = { player: {}, enemy: {}, ally: {} }
-  console.log(sprites)
+	sprites.pieces = { player: {}, enemy: {}, ally: {} }
+	console.log(sprites)
 
-  let palette = extract(sprites.piece, 0, 18, 3, 3)
-    .getContext("2d")
-    .getImageData(0, 0, 3, 3)
+	let palette = (_ => {
+		let piece = Canvas(sprites.piece.width, sprites.piece.height)
+		piece.drawImage(sprites.piece, 0, 0)
+		piece.getImageData(0, 18, 3, 3)
+	})()
 
-  let colors = {
-    white: [ 255, 255, 255, 255 ],
-    lightBlue: get(palette, 0, 0),
-    blue:      get(palette, 1, 0),
-    darkBlue:  get(palette, 2, 0)
-  }
+	let colors = {
+		white: [ 255, 255, 255, 255 ],
 
-  let palettes = {
-    player: [ get(palette, 0, 0), get(palette, 1, 0), get(palette, 2, 0) ],
-    enemy:  [ get(palette, 0, 1), get(palette, 1, 1), get(palette, 2, 1) ],
-    ally:   [ get(palette, 0, 2), get(palette, 1, 2), get(palette, 2, 2) ]
-  }
+		cyan: pixels.get(palette, 0, 0),
+		blue: pixels.get(palette, 1, 0),
+		navy: pixels.get(palette, 2, 0),
 
-  let equipments = [ "sword", "lance", "axe", "bow", "dagger", "shield", "hat" ]
-  for (let faction in palettes) {
-    let palette = palettes[faction]
-    for (let equipment of equipments) {
-      let source = sprites["symbols/" + equipment]
+		pink:   pixels.get(palette, 0, 1),
+		red:    pixels.get(palette, 1, 1),
+		purple: pixels.get(palette, 2, 1),
 
-      let piece = Canvas(16, 18)
-      let base = sprites.piece
-        .getContext("2d")
-        .getImageData(0, 0, 16, 18)
+		lime:  pixels.get(palette, 0, 2),
+		green: pixels.get(palette, 1, 2),
+		teal:  pixels.get(palette, 2, 2)
+	}
 
-      replace(base, colors.blue, palette[1])
-      replace(base, colors.darkBlue, palette[2])
-      piece.putImageData(base, 0, 0)
+	let palettes = {
+		player: [ colors.cyan, colors.blue, colors.navy ],
+		enemy:  [ colors.pink, colors.red, colors.purple ],
+		ally:   [ colors.lime, colors.green, colors.teal ]
+	}
 
-      let symbol = Canvas(source.width, source.height)
-      symbol.drawImage(source, 0, 0)
+	let equipments = [ "sword", "lance", "axe", "bow", "dagger", "shield", "hat" ]
+	for (let faction in palettes) {
+		let palette = palettes[faction]
+		for (let equipment of equipments) {
+			let source = sprites["symbols/" + equipment]
 
-      let template = symbol.getImageData(0, 0, source.width, source.height)
-      replace(template, colors.white, palette[0])
-      symbol.putImageData(template, 0, 0)
-      piece.drawImage(symbol.canvas, 4, 5)
+			let piece = Canvas(16, 18)
+			let base = sprites.piece
+				.getContext("2d")
+				.getImageData(0, 0, 16, 18)
 
-      replace(template, palette[0], palette[2])
-      symbol.putImageData(template, 0, 0)
-      piece.drawImage(symbol.canvas, 4, 4)
+			pixels.replace(base, colors.blue, palette[1])
+			pixels.replace(base, colors.darkBlue, palette[2])
+			piece.putImageData(base, 0, 0)
 
-      sprites.pieces[faction][equipment] = piece.canvas
-    }
-  }
+			let symbol = Canvas(source.width, source.height)
+			symbol.drawImage(source, 0, 0)
 
-  let view = View(map.layout.size[0] * 16, map.layout.size[1] * 16, sprites)
-  document.body.appendChild(view.context.canvas)
+			let template = symbol.getImageData(0, 0, source.width, source.height)
+			pixels.replace(template, colors.white, palette[0])
+			symbol.putImageData(template, 0, 0)
+			piece.drawImage(symbol.canvas, 4, 5)
 
-  View.render(view, state)
-  requestAnimationFrame(loop)
+			pixels.replace(template, palette[0], palette[2])
+			symbol.putImageData(template, 0, 0)
+			piece.drawImage(symbol.canvas, 4, 4)
 
-  function loop() {
-    View.render(view, state)
-    requestAnimationFrame(loop)
-  }
+			sprites.pieces[faction][equipment] = piece.canvas
+		}
+	}
 
-  window.addEventListener("mousemove", event => {
-    let canvas = view.context.canvas
-    if (event.target === canvas) {
-      cursor.position = scale(event.offsetX, event.offsetY)
-    }
-  })
+	let view = View(map.layout.size[0] * 16, map.layout.size[1] * 16, sprites)
+	document.body.appendChild(view.context.canvas)
 
-  window.addEventListener("mousedown", event => {
-    let canvas = view.context.canvas
-    if (event.target === canvas) {
-      if (!cursor.position) {
-        cursor.position = scale(event.offsetX, event.offsetY)
-      }
+	View.render(view, state)
+	requestAnimationFrame(loop)
 
-      let [ x, y ] = cursor.position
-      for (let i = 0; i < map.units.length; i++) {
-        let unit = map.units[i]
-        if (unit.position[0] === x && unit.position[1] === y) {
-          cursor.selection = i
-          break
-        }
-      }
-    }
-  })
+	function loop() {
+		View.render(view, state)
+		requestAnimationFrame(loop)
+	}
 
-  window.addEventListener("mouseup", event => {
-    if (cursor.selection !== null) {
-      cursor.selection = null
-    }
-  })
+	window.addEventListener("mousemove", event => {
+		let canvas = view.context.canvas
+		if (event.target === canvas) {
+			cursor.position = scale(event.offsetX, event.offsetY)
+		}
+	})
 
-  function scale(x, y) {
-    let canvas = view.context.canvas
-    let width = canvas.width
-    let height = canvas.height
-    return [ Math.floor(x / width * 16), Math.floor(y / height * 16) ]
-  }
+	window.addEventListener("mousedown", event => {
+		let canvas = view.context.canvas
+		if (event.target === canvas) {
+			if (!cursor.position) {
+				cursor.position = scale(event.offsetX, event.offsetY)
+			}
+
+			let [ x, y ] = cursor.position
+			for (let i = 0; i < map.units.length; i++) {
+				let unit = map.units[i]
+				if (unit.position[0] === x && unit.position[1] === y) {
+					cursor.selection = i
+					break
+				}
+			}
+		}
+	})
+
+	window.addEventListener("mouseup", event => {
+		if (cursor.selection !== null) {
+			cursor.selection = null
+		}
+	})
+
+	function scale(x, y) {
+		let canvas = view.context.canvas
+		let width = canvas.width
+		let height = canvas.height
+		return [ Math.floor(x / width * 16), Math.floor(y / height * 16) ]
+	}
 }
